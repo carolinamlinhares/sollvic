33a91bf42b3f3c1b26420d50ed8f205fe403ea19
Carolina Linhares <carolinacunha@MacBook-Pro-de-Carolina.local>
2017-03-19 18:42:51 -0300

    sad

--- scripts/designSq.js
+++ scripts/designSq.js
@@ -7,6 +7,5 @@ $(document).ready(function () {
         }
     });
     
-    
-    
+ 
 });
---
+++ scripts/joinUs.js
@@ -0,0 +1,87 @@
+$(function () {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            alert("Por favor complete todos os campos do formulário.")
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                // Formulario Join Us (contato@sollvic.com)
+                //https://docs.google.com/document/d/1e4jSvu6LjegqKCf5IAXC9fy9-WQtJbi2mbs4f9SgBpE/edit
+                url: "https://docs.google.com/forms/d/176CBydIpBXvcKKZnJTWUuOk9pGADyFP6i2ywS4AVigU/formResponse",
+                type: "POST",
+                data: {
+                    "entry.2022169595": name,
+                    "entry.1268399164": email,
+                    "entry.1688326692": phone,
+                    "entry.739605402": message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Sua mensagem foi enviada com sucesso. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function(xhr, status, error) {
+                    if(xhr.responseText != ""){
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que o servidor não está respondendo. Por favor tente novamente mais tarde ou use o e-mail de contato!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                    else{
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Sua mensagem foi enviada com sucesso. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
