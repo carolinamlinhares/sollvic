c94fdf51396e7e5515dc39071876ce874f5b86a7
Carolina Linhares <carolinacunha@MacBook-Pro-de-Carolina.local>
2017-04-09 12:05:09 -0300

    t4

--- scripts/joinUs.js
+++ scripts/joinUs.js
@@ -5,7 +5,7 @@ $(function () {
         submitError: function($form, event, errors) {
             alert("Por favor complete todos os campos obrigatórios do formulário.")
         },
-        submitSuccess: function($form, event) {
+        /*submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
@@ -19,8 +19,59 @@ $(function () {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+            $.ajax({
+                //url: "https://docs.google.com/forms/d/176CBydIpBXvcKKZnJTWUuOk9pGADyFP6i2ywS4AVigU/formResponse",
+                //url: "https://docs.google.com/forms/d/1FAIpQLSeAn25kS08xmX56GFF5y5WZULn7pPwFkS7tHKGDHBl9YCwbWw/formResponse",
+                type: "POST",
+                data: {
+                    "entry.1785861497": name,
+                    "entry.719194490": course,
+                    "entry.1273701555": subject,
+                    "entry.1943849180": email,
+                    "entry.868553239": phone,
+                    "entry.376576089": message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Sua mensagem foi enviada com sucesso. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
 
-        },
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function(xhr, status, error) {
+                    if(xhr.responseText != ""){
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que o servidor não está respondendo. Por favor tente novamente mais tarde ou use o e-mail de contato!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                    else{
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Sua mensagem foi enviada com sucesso. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
+                },
+            })
+        },*/
         filter: function() {
             return $(this).is(":visible");
         },
