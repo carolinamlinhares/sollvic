fd536ea21b5e0057fad163518b7d17d7ca56b19b
julianaliborio <juliana.liboriolima@gmail.com>
2017-04-13 07:06:13 -0300

    Update reportCheckCD.js

--- scripts/reportCheckCD.js
+++ scripts/reportCheckCD.js
@@ -51,6 +51,12 @@ $(document).ready(function() {
     textC3 = "<p> 3º caso – A viga encontra-se no domínio 4, e foi então reprovada neste critério. A mesma precisa ser duplamente armada de modo a passar para o Domínio 3. Uma parcela dos esforços de compressão será então resistida pelo aço inserido na zona comprimida da seção. Assim, procedem as seguintes etapas de cálculo:<br><br>3.1 - Cálculo de uma nova posição da LN.<br><br>Tomando-se a altura máxima que a linha neutra pode atingir para estar de acordo com os critérios da NBR 6118:2014, valor que corresponde à 45% da altura útil, tem-se:<br><br>\\(\\ x' = 0.45 * d =\\ \\)" + resultado.xd + " cm<br><br>Calcula-se então o valor da primeira parcela do momento fletor resistente, a ser gerado pela área de aço tracionada:<br><br>\\(\\ M_{1d} = 0.68 * bw * x_d * f_{cd} * (d - 0.4 * x_d) \\)<br><br>\\(\\ M_{1d} =\\ \\)" + resultado.m1d + " kN.cm<br><br>A segunda parcela do momento fletor resistente, a ser gerado pela área de aço na zona comprimida é calculado através da subtração:<br><br>\\(\\ M_{2d} = M_d - M_{1d} \\).<br><br>\\(\\ M_{2d} =\\ \\)" + resultado.m2d + " kN.cm<br><br>onde Md é o momento de flexão total solicitado, já majorado.<br><br>A área de aço comprimida necessária As` é então dada pela equação:<br><br>\\(\\ A'_{s} =\\ \\frac{M_{2d}}{ \\sigma'_{sd} * (d - d'c)}\\)<br>\\(\\ A'_s =\\ \\)" + resultado.asl + " cm²<br><br>\\(\\ \\sigma'_{sd}\\) – Tensão de escoamento do aço;<br>\\(\\ d'\\) – Distância da borda mais comprimida ao centro de gravidade da armadura comprimida;<br><br>Para encontrar o valor de \\(\\ \\sigma'_{sd}\\) e substituir na equação acima, calcula-se o valor da deformação da armadura comprimida através da equação:<br><br>\\(\\ \\varepsilon'_{sd} = 3.5 * (1 - \\frac{1}{0.45} * \\frac{d'c}{d}) =\\ \\)" + resultado.elsd + " cm <br><br>Multiplicando-se o seu valor pelo módulo de elasticidade do aço (210GPa), encontra-se a tensão de escoamento, que na equação acima, resultará a área de aço comprimida:<br><br>\\(\\ \\sigma'_{sd} = (\\frac{\\varepsilon'_{sd}}{10}) * (\\frac{Es}{100}) = \\)" + resultado.tlsd + " cm<br><br>\\(\\ E_s = \\) 210GPa - Módulo de elasticidade do aço;<br><br>A área de aço tracionada necessária é a soma de duas parciais \\(\\ A_{s1}\\) e \\(\\ A_{s2} \\). A primeira equilibra o momento fletor resistente da área de concreto comprimido e a segunda faz o equilíbrio com o momento fletor da armadura comprimida. Os cálculos são dados pelas equações abaixo, onde \\(\\ A_{st}\\) é a área de aço total necessária na zona tracionada.<br><br>\\(\\ A_{s1} = \\frac{M_{1d}}{\\sigma_sd * (d - 0.4 * x_d)} = \\)" + resultado.as1 + " cm²<br><br>\\(\\ A_{s2} = \\frac{M_{2d}}{\\sigma_sd * (d - d'_c)} = \\)" + resultado.as2 + " cm²<br><br>\\(\\ A_{st} = A_{s1} + A_{s2} = \\)" + resultado.ast + " cm²<br><br> Após os cálculos e detalhamento da armadura, deve-se verificar o novo valor da altura útil \\(\\ d\\).<br><br></p>";
     MathJax.Hub.Queue(["Typeset", MathJax.Hub, textC3]);
     
+    tableT1 = "<p>" + "<table class='reportable'><tr><th> Tipo de Viga </th><td> <span class='situationS'> </span></p>" + resultado.situationS + " </td></tr><tr><th> Altura Útil \\({(d)}\\) </th><td> <span class='d'> </span></p>" + resultado.d + " cm </td></tr><tr><th> Posição da Linha Neutra \\({(x)}\\) </th><td> <span class='x'> </span></p>" + resultado.x + " cm </td></tr><tr><th> Domínio de Deformação </th><td> <span class='dominio'> </span></p>" + resultado.dominio + " </td></tr><tr><th> Relação \\({(x/d)}\\) </th><td> <span class='ln'> </span></p>" + resultado.ln + " </td></tr><tr><th> Momento Fletor Majorado \\(M_{sd}\\) </th><td> <span class='md'> </span></p>" + resultado.md + "  kN.cm</td></tr><tr><th> Área de Aço Necessária \\(A_{s}\\) </th><td> <span class='as'> </span></p>" + resultado.as + "  cm²</td></tr><tr><th> Armadura de Pele \\(A_{sp}\) </th><td> <span class='resultArmPele'> </span></p>" + resultado.resultArmPele + " </td></tr></table></p>";
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, tableT1]);
+    
+    tableT2 = "<p>" + "<table class='reportable'><tr><th> Tipo de Viga </th><td> <span class='situationS'> </span></p>" + resultado.situationS + " </td></tr><tr><th> Altura Útil \\({(d)}\\) </th><td> <span class='d'> </span></p>" + resultado.d + " cm </td></tr><tr><th> Posição da Linha Neutra \\({(x_d)}\\) </th><td> <span class='xd'> </span></p>" + resultado.xd + " cm </td></tr><tr><th> Domínio de Deformação </th><td> <span class='dominio'> </span></p>" + resultado.dominio + " </td></tr><tr><th> Relação \\({(x/d)}\\) </th><td> <span class='lnd'> </span></p>" + resultado.lnd + " </td></tr><tr><th> Momento Fletor Majorado \\(M_{sd}\\) </th><td> <span class='md'> </span></p>" + resultado.md + " kN.cm </td></tr><tr><th> Área de Aço Tracionada Necessária \\(A_{st}\\) </th><td> <span class='ast'> </span></p>" + resultado.ast + " cm² </td></tr><tr><th>Área de Aço Comprimida Necessária \\(A_{s'}\\) </th><td> <span class='asl'> </span></p>" + resultado.asl + " cm² </td></tr><tr><th> Armadura de Pele\\(A_{sp}\\) </th><td> <span class='resultArmPele'> </span></p>" + resultado.resultArmPele + " </td></tr></table></p>";
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, tableT2]);
+    
     
     // Header
     $(".project").html(resultado.project);
@@ -116,16 +122,16 @@ $(document).ready(function() {
     $(".astTol").html(resultado.astTol);
     $(".aslTol").html(resultado.aslTol);
     $(".caseS").html(resultado.caseS);
+    $(".caseT").html(resultado.caseT);
     
  
     
-    //Results Case
+    //Results CaseS
     $("#textC1").html(resultado.textC1);
     $("#textC2").html(resultado.textC2);
     $("#textC3").html(resultado.textC3);
         
-    
-    // Case
+    // CaseS
     switch (resultado.caseS) {
         case "Caso 1":
             $("#textC").html(textC1);
@@ -138,14 +144,21 @@ $(document).ready(function() {
             break;
       }
     
-    //Arranjos Table
     
-    $('#suggestion_table').append('<table style="width: 100%"></table>');
-    var table = $('#suggestion_table').children();
-    table.append(tableHeader);
-    for (i = 0; i <= resultado.arranjos.length; i += 1) {
-        table.append('<tr><td>' +  resultado.arranjos[i].qtd + '</td>' + '<td>' +  resultado.arranjos[i].bitola + '</td>' +'<td>' +  resultado.arranjos[i].ncam + '</td>' + '<td>' +  resultado.arranjos[i].as + '</td>' + '<td>' +  resultado.arranjos[i].taxa + '</td>' + '<td>' +  resultado.arranjos[i].ah + '</td>' + '<td>' +  resultado.arranjos[i].av + '</td>' + '</tr>');
-    }
+    //Results CaseT
+    $("#tableT1").html(resultado.tableT1);
+    $("#tableT2").html(resultado.tableT2);
+        
+    // CaseT
+    switch (resultado.caseT) {
+        case "Tabela Simples":
+            $("#tableT").html(tableT1);
+            break;
+        case "Tabela Dupla":
+            $("#tableT").html(tableT2);
+            break;
+      }
+    
     
     
 });
